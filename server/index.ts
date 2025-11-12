64d7b28fbd1eb65f4284020d77e8092a605a88c0
Ryosei Fukushima <ryosei-f@weseek.co.jp>
2025-11-12 20:49:47 +0900

    MongoDBによるデータ永続化の実装

--- server/index.ts
+++ server/index.ts
@@ -1,35 +1,73 @@
 import express, {Request, Response} from "express";
-import cors from "cors"; // 違うドメインから来たリクエストを許容するためのもの。
+import cors from "cors"; // 違うドメイン(nextjsなど)から来たリクエストを許容するためのもの。
+import { MongoClient, ObjectId, Collection } from "mongodb";
+
+export type Subject = "math" | "science" | "programing";
+export type Word = {
+  id: string;
+  englishWord: string;
+  japaneseWord: string;
+  subject: Subject;
+};
 
 const app = express();
 app.use(cors());
 app.use(express.json()); // json文字列でフロントからデータが届く。expressでjson文字列を読むために必要。
 
-let words: any[] = [
-    { id: "1", englishWord: "apple", japaneseWord: "りんご", subject: "science" },
-    { id: "2", englishWord: "banana", japaneseWord: "バナナ", subject: "science" },
-]; // 一時的に保存するメモリ配列
+const client = new MongoClient("mongodb://localhost:27017");
+const dbName = "wordknock";
+let wordsCollection: Collection<Word>;
 
-// 単語一覧を返す
-app.get("/api/words", (req: Request, res: Response) => {
+async function connectDB() {
+  await client.connect();
+  console.log("MongoDB connected");
+  wordsCollection = client.db(dbName).collection<Word>("words");
+
+  app.listen(3333, () => {
+    console.log("🚀 API Server is running on http://localhost:3333");
+  });
+}
+
+app.get("/api/words", async (rep: Request, res: Response) => {
+  const words = await wordsCollection.find().toArray();
   res.json(words);
 });
 
 // 単語を追加する
-app.post("/api/words", (req: Request, res: Response) => {
-  const newWord = req.body;
-  words.push(newWord);
-  res.status(201).json(newWord);
+app.post("/api/words", async(req: Request, res: Response) => {
+  try {
+    const newWord = req.body;
+    if (!newWord.japaneseWord || !newWord.englishWord || !newWord.subject) {
+      return res.status(400).json({ error: "必須項目が不足しています" });
+    }
+    const result = await wordsCollection.insertOne(newWord);
+    res.status(201).json({ ...newWord, _id: result.insertedId });
+  } catch (err) {
+    console.log("Error inserting word:", err);
+  }
 });
 
 // 単語を削除する
-app.delete("/api/words/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  words = words.filter(w => w.id !== id);
-  res.status(204).send();
+app.delete("/api/words/:id", async(req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ error: "IDが必要です" });
+    }
+    const result = await wordsCollection.deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) {
+      return res.json(404).json({ error: "単語が見つかりません。削除できませんでした。" });
+    }
+    res.status(204).send();
+  } catch (err) {
+    console.log("Error deleting word: ", err);
+    res.status(500).json({ error: "サーバーエラー" });
+  }
 });
 
 // サーバー起動
-app.listen(3333, () => {
-  console.log("🚀 API Server is running on http://localhost:3333");
+connectDB().then(() => {
+  app.listen(3333, () => {
+    console.log("API Server is running on http://localhost:3333")
+  })
 });
